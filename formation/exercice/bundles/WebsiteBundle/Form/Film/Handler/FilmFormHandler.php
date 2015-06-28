2a66ce3f0096bb21c83c252385a48ed471d106c7
alhassane <etienne.delongeaux@gmail.com>
2015-06-28 13:58:52 +0200

    add formation

---
+++ formation/exercice/bundles/WebsiteBundle/Form/Film/Handler/FilmFormHandler.php
@@ -0,0 +1,32 @@
+<?php
+namespace Dirisi\WebsiteBundle\Form\Film\Handler;
+
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Dirisi\WebsiteBundle\Form\Handler\AbstractFormHandler;
+use Dirisi\WebsiteBundle\Manager\FilmManager;
+
+class FilmFormHandler extends AbstractFormHandler
+{
+    private $message = "";
+    
+    public function __construct(FormInterface $form, FilmManager $processManager, Request $request) {
+        parent::__construct($form, $request, $processManager);      
+    }
+
+    protected function getValidMethods()
+    {
+        return array('POST');
+    }
+  
+    protected function onSuccess()
+    {
+        $this->message = $this->processManager->saveFormProcess($this->form->getData());
+        
+        return true;
+    }
+    
+    public function getMessage() {
+        return $this->message;
+    }
+}
