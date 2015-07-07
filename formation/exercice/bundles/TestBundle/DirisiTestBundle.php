5843895f992637e62628f96ec7cfdafcf15d03af
alhassane <etienne.delongeaux@gmail.com>
2015-07-07 11:44:45 +0200

    debug

--- formation/exercice/bundles/TestBundle/DirisiTestBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dirisi\TestBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DirisiTestBundle extends Bundle
-{
-}
--- formation/exercice/bundles/WebsiteBundle/DirisiWebsiteBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Dirisi\WebsiteBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class DirisiWebsiteBundle extends Bundle
-{
-}
